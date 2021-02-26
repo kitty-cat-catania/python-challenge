e1450c8c06677f74f21eef52a7fcc4ffca0d38df
Gabrielle N Catania <goobah@Gabrielles-MacBook.local>
2021-02-26 15:52:51 -0800

    pybank stuff

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,7 @@
 import csv
 import os 
+from statistics import mean
+import math
 
 
 current_directory = os.path.dirname(__file__)
@@ -19,10 +21,53 @@ with open(budget_data) as handle:
 
     #create list variable to hold months in 
     months = []
+    #create list variable to hold profit of each month
+    monthProf = []
+    #create int variable to hold net total of profits/losses
+    total = 0
     #create for-loop that loops through all rows in file
     for row in reader:
         #prints ['Month-Year','Profits/Losses']
         print(row)
         months.append(row[0])
+       
+        #casts the profit as an integer and stores in profLoss
+        profLoss = int(row[1])
+        monthProf.append(int(row[1]))
+        #adds profit of that row to total
+        total = total +profLoss
     #prints the length of the array 'months' -- equals 86
     print((len(months)))
+    #prints total profit
+    print(total)
+    print(len(monthProf))
+    aveCh = list()
+    for i in range (85):
+        aveCh.insert(i," ")
+    print(len(aveCh))
+    for j in range(85):
+        change =int(monthProf[j+1] - monthProf[(j)])
+        aveCh[j]=change
+    print(aveCh)
+    print(len(aveCh))
+    aveAveCh= (sum(aveCh)/len(aveCh))
+    print(aveAveCh)
+    maxCh = max(aveCh)
+    minCh= min(aveCh)
+
+    print(minCh)
+    print(maxCh)
+
+    #opens/creats analysis.txt file to write output to
+    output_handle = open("analysis.txt","w+")
+
+    output_handle.write("Financial Analysis")
+    output_handle.write("\n----------------------------\n")
+    output_handle.write("Total Months: " + str(len(months)))
+    output_handle.write("\nTotal: " + str(total))
+    output_handle.write("\nAverage Change: " + str(aveAveCh))
+    output_handle.write("\nGreatest increase in profits: " + str(maxCh))
+    output_handle.write("\nGreatest decrease in profits: "+ str(minCh))
+
+
+    
